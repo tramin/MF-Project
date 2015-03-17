908c505a36360f961f391003456768ae70fe92a5
tramin <clicktam1@gmail.com>
2015-03-17 20:16:58 +0700

    Update DB_Connect.php

--- DB_Connect.php
+++ DB_Connect.php
@@ -1,15 +1,5 @@
 <?php
 @mysql_connect("localhost","root","3131") or die(mysql_error());
 @mysql_select_db("in_pro") or die (mysql_error());
-<<<<<<< HEAD
 @mysql_query("SET NAMES utf8");
-
-/*$connect = mysql_connect('localhost', 'root', 3131);
-if($connect){
-	$connect_db = mysql_select_db('io878o0o');
-}else{
-	echo "Can not conected database";
-}*/
-=======
->>>>>>> b77819c27f20fcf2ceae6f7077e2d64098e21bfb
 ?>
